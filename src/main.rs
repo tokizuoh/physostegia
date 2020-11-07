0215381c2354463be2ce3fb8c02e0c19faff9fab
tokizuoh <k_sakai@yumemi.co.jp>
2020-11-07 17:15:48 +0900

    入力受付時のメッセージ文の改行をdisableに

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::io;
+use std::io::Write;
+use colored::*;
 use regex::Regex;
 use structopt::StructOpt;
 use anyhow::{Context, Result};
@@ -8,6 +11,15 @@ struct Argument {
     path: std::path::PathBuf,
 }
 
+fn input(message: &str) -> String {
+    print!("{}: ", message);
+    io::stdout().flush().unwrap(); 
+
+    let mut word = String::new();
+    io::stdin().read_line(&mut word).ok();
+    word.trim().to_string()
+}
+
 fn main() -> Result<()> {
     // 入力受付
     let args = Argument::from_args();
@@ -15,13 +27,19 @@ fn main() -> Result<()> {
         .with_context(|| format!("could not read file `{:p}`", &args.path))?;
     
     // TODO: マッチさせたい正規表現を生成
+    // loop {
+    //     let a = input("入力させたい");
+    //     println!("Result: {}", a.bold());
+    //     if a == "q" {
+    //         break;
+    //     }
+    // }
 
     // 一行ずつ呼んで処理
     for line in content.lines() {
         println!("{}", line);
     }
         
-
     // let _words = r"^\d{4}";
     // let re = Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap();
     // let re2 = Regex::new(r"^\d{4}-\d{2}-\d{2}").unwrap();
